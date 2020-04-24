964ded3480dd1eb9316110eafd3cfb9df418fa5b
tanakashun <michael021212@gmail.com>
2020-04-24 19:43:55 +0900

    implement action and action creater

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+export const increment = () => {{
+    type: INCRIMENT
+}}
+
+export const decrement = () => {
+  return {
+    type: DECRIMENT
+  }
+}
